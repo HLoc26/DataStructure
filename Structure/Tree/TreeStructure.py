ee74ac1349439a716888e6ba20dec882d3d54918
HLoc26 <dhl26052004@gmail.com>
2024-08-15 04:45:14 +0700

    Added main.py

--- Structure/Tree/TreeStructure.py
+++ Structure/Tree/TreeStructure.py
@@ -83,35 +83,3 @@ class Tree():
             if node.nodeName == target:
                 return [True, trace]
         return [False, None]
-
-    
-
-'''
-           A
-        /  |  \
-       B   C    D
-      /  / | \  | \
-     E  F  G  H I  J
-  '''
-
-tree = Tree("A")
-
-tree.AddChildren(tree.root, TreeNode("B"))
-tree.AddChildren(tree.root, TreeNode("C"))
-tree.AddChildren(tree.root, TreeNode("D"))
-
-# Add node to node B
-tree.AddChildren(tree.root.children[0], TreeNode("E"))
-
-# Add node to node C
-tree.AddChildren(tree.root.children[1], TreeNode("F"))
-tree.AddChildren(tree.root.children[1], TreeNode("G"))
-tree.AddChildren(tree.root.children[1], TreeNode("H"))
-
-# Add node to node D
-tree.AddChildren(tree.root.children[2], TreeNode("I"))
-tree.AddChildren(tree.root.children[2], TreeNode("J"))
-
-
-print(tree.BFS("J"))
-print(tree.BFS("J"))
---
+++ Structure/Tree/main.py
@@ -0,0 +1,34 @@
+from TreeStructure import Tree
+from TreeNode import TreeNode
+
+'''
+           A
+        /  |  \
+       B   C    D
+      /  / | \  | \
+     E  F  G  H I  J
+  '''
+
+tree = Tree("A")
+
+tree.AddChildren(tree.root, TreeNode("B"))
+tree.AddChildren(tree.root, TreeNode("C"))
+tree.AddChildren(tree.root, TreeNode("D"))
+
+# Add node to node B
+tree.AddChildren(tree.root.children[0], TreeNode("E"))
+
+# Add node to node C
+tree.AddChildren(tree.root.children[1], TreeNode("F"))
+tree.AddChildren(tree.root.children[1], TreeNode("G"))
+tree.AddChildren(tree.root.children[1], TreeNode("H"))
+
+# Add node to node D
+tree.AddChildren(tree.root.children[2], TreeNode("I"))
+tree.AddChildren(tree.root.children[2], TreeNode("J"))
+
+
+print(tree.BFS("J"))
+tree.RenameNode("J", "newJ")
+print(tree.BFS("J"))
+print(tree.BFS("newJ"))
