95e7cdc83726587495f2351e4e0193f8d10aea3d
HLoc26 <dhl26052004@gmail.com>
2024-08-17 14:08:27 +0700

    Modified

--- Tree/TreeNode.py
+++
@@ -1,7 +0,0 @@
-class TreeNode():
-    def __init__(self, nodeName: str) -> None:
-        self.nodeName: str = nodeName
-        self.children: list[TreeNode] = []
-
-    def __repr__(self) -> str:
-        return self.nodeName
